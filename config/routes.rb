686219681db711a490814bd016dbae0e667274a6
Juliette Hutton <juliettehutton2@gmail.com>
2024-05-23 12:40:10 -0400

    created Lists controller and action, did validation, created Bookmarks controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'bookmarks/new'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
@@ -7,4 +8,11 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+  get 'lists', to: 'lists#index'
+
+  get 'lists/new', to: 'lists#new', as: :new_list
+
+  get 'lists/:id', to: 'lists#show', as: :list
+
+  post 'lists', to: 'lists#create'
 end
