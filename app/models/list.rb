123c86085ff46d995654e0acc1066d76f75e0093
Juliette Hutton <juliettehutton2@gmail.com>
2024-05-24 16:13:36 -0400

    added the photo in the form

--- app/models/list.rb
+++ app/models/list.rb
@@ -2,4 +2,5 @@ class List < ApplicationRecord
   validates :name, uniqueness: true, presence: true
   has_many :bookmarks
   has_many :movies, dependent: :destroy, through: :bookmarks
+  has_one_attached :photo
 end
